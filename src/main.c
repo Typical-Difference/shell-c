f5f440c56840f7a958cbaa55be050f8c3b3c53d1
Balaji <104999488+Typical-Difference@users.noreply.github.com>
2025-11-05 05:13:01 +0530

    Invalid command message

--- src/main.c
+++ src/main.c
@@ -7,6 +7,13 @@ int main(int argc, char *argv[]) {
 
   // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
+  char command[1024];
+  fgets(command, sizeof(command), stdin);
+  command[strcspn(command, '\n')] = '\0'; // Remove the last new line character after user inputs ENTER
+
+  //print error for invalid messages
+  printf("%s: command not found\n", command);
+
 
   return 0;
 }
