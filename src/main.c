89e250ecc1d87f164d7309e723ed05143633cea8
Balaji <104999488+Typical-Difference@users.noreply.github.com>
2025-11-06 18:49:44 +0530

    Loop shell REPL

--- src/main.c
+++ src/main.c
@@ -3,18 +3,20 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  // Flush after every printf
-  setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
-  char command[1024];
-  fgets(command, sizeof(command), stdin);
-  command[strcspn(command, "\n")] = '\0'; // Remove the last new line character after user inputs ENTER
+    // Flush after every printf
+    setbuf(stdout, NULL);
+  while(1){
 
-  //print error for invalid messages
-  printf("%s: command not found\n", command);
+    // TODO: Uncomment the code below to pass the first stage
+    printf("$ ");
+    char command[1024];
+    fgets(command, sizeof(command), stdin);
+    command[strcspn(command, "\n")] = '\0'; // Remove the last new line character after user inputs ENTER
 
-
+    //print error for invalid messages
+    printf("%s: command not found\n", command);
+  }
+  
   return 0;
 }
