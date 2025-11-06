a791f34a0384bf2cc0e7edfe7bc6369486eb31fe
Balaji <104999488+Typical-Difference@users.noreply.github.com>
2025-11-06 21:28:23 +0530

    exit command

--- src/main.c
+++ src/main.c
@@ -3,11 +3,12 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+  int running_flag = 1;
 
+  while(running_flag){
     // Flush after every printf
     setbuf(stdout, NULL);
-  while(1){
-
+    
     // TODO: Uncomment the code below to pass the first stage
     printf("$ ");
     char command[1024];
@@ -15,7 +16,12 @@ int main(int argc, char *argv[]) {
     command[strcspn(command, "\n")] = '\0'; // Remove the last new line character after user inputs ENTER
 
     //print error for invalid messages
-    printf("%s: command not found\n", command);
+    if(strncmp("exit", command, 4) && isdigit(command[4] - ' ')){
+      running_flag = 0;
+    }
+    else{
+      printf("%s: command not found", command);
+    }
   }
   
   return 0;
